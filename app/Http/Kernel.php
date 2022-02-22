73d642d3a156633c4299422fcf965a0955aa0045
HumbertoCanales <19170146@uttcampus.edu.mx>
2022-02-22 13:48:47 -0600

    [ADD] Changed controllers, views, and middlewares

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -59,10 +59,10 @@ class Kernel extends HttpKernel
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         '2fa' => \App\Http\Middleware\Check2FA::class,
+        'act2fa' => \App\Http\Middleware\Active2FA::class,
     ];
 }
